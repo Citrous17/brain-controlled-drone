e5db0ccfeab952e6d1053c350b12cc9a09300509
Kyle Stallings <citrous@tamu.edu>
2025-03-02 21:00:21 -0600

    Added Graph Information
    Also has pictures and videos

--- brain-controlled-drone-app/app/page.tsx
+++ brain-controlled-drone-app/app/page.tsx
@@ -1,9 +1,10 @@
 import LandingPage from '@/app/ui/LandingPage';
 import test from 'node:test';
-import { testDb } from './lib/actions';
+import { testDb, getData } from './lib/actions';
 
-export default function Home() {
+export default async function Home() {
   testDb();
+
   return (
     <LandingPage />
   )
